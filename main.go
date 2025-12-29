1d1794a189f2bb76b5667930eae109f6aee7997a
Steve Zabka <info@cryinkfly.de>
2025-12-29 11:39:22 +0100

    Implement user extraction from BoltDB

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	bolt "go.etcd.io/bbolt"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func main() {
+	dbPath := "/db/database.db"
+	outPath := "/config/users"
+
+	db, err := bolt.Open(dbPath, 0600, &bolt.Options{ReadOnly: true})
+	if err != nil {
+		log.Fatalf("DB open failed: %v", err)
+	}
+	defer db.Close()
+
+	out, err := os.Create(outPath)
+	if err != nil {
+		log.Fatalf("users create failed: %v", err)
+	}
+	defer out.Close()
+
+	w := bufio.NewWriter(out)
+	defer w.Flush()
+
+	seen := map[string]bool{}
+	count := 0
+
+	err = db.View(func(tx *bolt.Tx) error {
+		return tx.ForEach(func(_ []byte, b *bolt.Bucket) error {
+			return b.ForEach(func(_, v []byte) error {
+				var u User
+				if json.Unmarshal(v, &u) != nil {
+					return nil
+				}
+				if u.Username == "" || u.Password == "" {
+					return nil
+				}
+				if !strings.HasPrefix(u.Password, "$2") {
+					return nil
+				}
+				if seen[u.Username] {
+					return nil
+				}
+
+				fmt.Fprintf(w, "%s:%s\n", u.Username, u.Password)
+				seen[u.Username] = true
+				count++
+				return nil
+			})
+		})
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("users file written (%d users)\n", count)
+}
